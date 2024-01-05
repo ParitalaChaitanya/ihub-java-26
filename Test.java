3cededef8aab534315dca424b9b04a2f3a497745
ParitalaChaitanya <155723143+ParitalaChaitanya@users.noreply.github.com>
2024-01-05 17:01:06 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,22 @@
+class Test  
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(10 > 20); // false 
+
+		System.out.println(10 >= 20); // false 
+
+		System.out.println(10 < 20); //true 
+
+		System.out.println( 10 <= 10); // true 
+
+		System.out.println(10 == 10); // true 
+
+		System.out.println(10 == 20); // false 
+
+		System.out.println(10 != 20); // true 
+
+		System.out.println(10 != 10); // false 
+		
+	}
+}
